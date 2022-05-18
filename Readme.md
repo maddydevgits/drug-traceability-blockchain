294d006dd66213078e120403eb8bc5120991c705
madblocksgit <madblocks2k19@gmail.com>
2022-05-18 21:33:28 +0530

    tested and validated the login and register pages

--- Readme.md
+++ Readme.md
@@ -9,4 +9,5 @@ Tracking supplies through this network is non-trivial due to several factors inc
 1. Create a Login and Register for all roles :heavy_check_mark:
 2. Create a Smart Contract for login and register :heavy_check_mark:
 3. Create a Migration Script for deploying register smart contract :heavy_check_mark:
-4. Test, and Validate the Login and Register pages with Smart Contract :x:
+4. Test, and Validate the Login and Register pages with Smart Contract :heavy_check_mark:
+5. Create a Lab Dashboard :x:
--- src/app.py
+++ src/app.py
@@ -1,7 +1,25 @@
 from flask import Flask,render_template,request,redirect,session
+from web3 import Web3,HTTPProvider
+import json
+
+def connect_Blockchain_register(acc):
+    blockchain_address="http://127.0.0.1:8545"
+    web3=Web3(HTTPProvider(blockchain_address))
+    if(acc==0):
+        acc=web3.eth.accounts[0]
+    web3.eth.defaultAccount=acc
+    artifact_path='../build/contracts/register.json'
+    contract_address="0x02E5556090ba5e6ab9Ee89b5150183E960eed4Eb"
+    with open(artifact_path) as f:
+        contract_json=json.load(f)
+        contract_abi=contract_json['abi']
+
+    contract=web3.eth.contract(address=contract_address,abi=contract_abi)
+    print('connected with blockchain')
+    return (contract,web3)
 
 app=Flask(__name__)
-# app.secret_key('makeskilled')
+app.secret_key='makeskilled'
 
 @app.route('/')
 def indexPage():
@@ -18,18 +36,28 @@ def loginPage():
 @app.route('/registerUser',methods=['GET','POST'])
 def registerUser():
     name=request.form['name']
-    role=request.form['role']
+    role=int(request.form['role'])
     walletaddr=request.form['walletaddr']
-    password=request.form['password']
+    password=int(request.form['password'])
     print(walletaddr,password,name,role)
+    contract,web3=connect_Blockchain_register(walletaddr)
+    tx_hash=contract.functions.registerUser(walletaddr,password,name,role).transact()
+    web3.eth.waitForTransactionReceipt(tx_hash)
     return redirect('/login')
 
 @app.route('/loginUser',methods=['GET','POST'])
 def loginUser():
     walletaddr=request.form['walletaddr']
-    password=request.form['login']
+    password=int(request.form['password'])
     print(walletaddr,password)
-    return redirect('/dashboard')
+    contract,web3=connect_Blockchain_register(walletaddr)
+    state=contract.functions.loginUser(walletaddr,password).call()
+    print(state)
+    if(state=='True'):
+        session['walletaddr']=walletaddr
+        return redirect('/dashboard')
+    else:
+        return redirect('/login')
 
 if(__name__=="__main__"):
     app.run(debug=True)
